044e0a52084e9afe116994c7687d324a325b3cc5
Alexey <42065561+exxodia666@users.noreply.github.com>
2022-01-25 21:32:06 +0200

    init

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import HomeScreen from './src/screens/HomeScreen';
+import 'react-native-gesture-handler'
+
+const App = () => {
+  return (
+    <>
+      <HomeScreen />
+    </>
+
+  );
+};
+
+
+
+export default App;
