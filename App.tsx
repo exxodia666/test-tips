522cc7615eb67719d935fdd508b5c6aed2097401
Alexey <42065561+exxodia666@users.noreply.github.com>
2022-01-26 13:46:55 +0200

    +packages

--- App.tsx
+++ App.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import HomeScreen from './src/screens/HomeScreen';
 import 'react-native-gesture-handler'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { store, persistor } from './src/store/store'
 
-const App = () => {
-  return (
-    <>
+const App = () =>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <HomeScreen />
-    </>
-
-  );
-};
-
-
+    </PersistGate>
+  </Provider>
 
 export default App;
